990c0eccb75103722e424a6f2ee1a0a3d939d8ff
Ria Kakarlapudi <ria.kakarlapudi@gmail.com>
2024-03-28 10:09:15 -0400

    initial commit

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,34 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is your favorite subject in school?");
+            string favSubject = Console.ReadLine();
+
+            switch(favSubject) {
+
+                case "English":
+                Console.WriteLine("English - not my favorite");
+                break;
+
+                case "Science":
+                Console.WriteLine("Science - not my favorite");
+                break;
+
+                case "History":
+                Console.WriteLine("History - not my favorite");
+                break;
+
+                case "Accounting":
+                Console.WriteLine("Accounting- not my favorite");
+                break;
+
+                case "Math":
+                Console.WriteLine("Math- not my favorite");
+                break;
+
+            }
+
+
         }
     }
 }
